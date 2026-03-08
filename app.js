8ed534a0bc138be5a6fb12050b82302dff353d98
Urvashi <24bai70244@cuchd.in>
2026-03-08 21:05:20 +0530

    Initial commit - productCatalog project

--- app.js
+++ app.js
@@ -1,56 +1,27 @@
 const mongoose = require("mongoose");
-const Product = require("./models/Product");
+const Product = require("./models/Product"); // your Product schema
 
-mongoose.connect("mongodb://127.0.0.1:27017/productDB")
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+const uri = "mongodb+srv://24bai70246_db_user:7vltvPpfTYMps1XN@cluster0.dgbyefh.mongodb.net/productDB?retryWrites=true&w=majority";
+
+mongoose.connect(uri)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log("Connection Error:", err));
 
 async function run() {
-
   const product = new Product({
     name: "Premium Headphones",
     category: "Electronics",
     variants: [
-      {
-        sku: "HP-BL-001",
-        color: "Black",
-        price: 199.99,
-        stock: 15
-      },
-      {
-        sku: "HP-WH-001",
-        color: "White",
-        price: 209.99,
-        stock: 8
-      }
+      { sku: "HP-BL-001", color: "Black", price: 199.99, stock: 15 },
+      { sku: "HP-WH-001", color: "White", price: 209.99, stock: 8 }
     ],
     reviews: [
-      {
-        userId: new mongoose.Types.ObjectId(),
-        rating: 5,
-        comment: "Excellent sound quality"
-      }
+      { userId: new mongoose.Types.ObjectId(), rating: 5, comment: "Excellent sound quality" }
     ]
   });
 
-  await product.save();
-
+  await product.save(); // this actually creates the database and collection
   console.log("Product Saved");
-
-  // Aggregation (Average Rating)
-
-  const result = await Product.aggregate([
-    { $unwind: "$reviews" },
-    {
-      $group: {
-        _id: "$_id",
-        avgRating: { $avg: "$reviews.rating" }
-      }
-    }
-  ]);
-
-  console.log("Average Rating:", result);
-
 }
 
 run();
